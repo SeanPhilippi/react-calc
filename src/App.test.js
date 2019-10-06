bdbf7f968445933633f47a137b0cd7a51bf06b5d
Sean Philippi <sean.philippi@gmail.com>
2019-10-06 16:01:29 -0500

    calculates unlimited number of numbers

--- src/App.test.js
+++ src/App.test.js
@@ -31,4 +31,7 @@ test('calculate', () => {
   expect(calculate('3,3')).toBe(6);
   expect(calculate('rfr,45')).toBe(45);
   expect(calculate('2,30')).toBe(32);
+  expect(calculate('1,2,3,4,5,6,7,8,9,10,11,12')).toBe(78);
+  expect(calculate('65,4,34,3,654,34,52')).toBe(846);
+  expect(calculate('65,4,34,4vrv,se,34,52')).toBe(189);
 });
--- src/utilities/calculate.js
+++ src/utilities/calculate.js
@@ -2,10 +2,8 @@ import getNum from './getNum';
 
 const calculate = inputString => {
   const values = inputString.split(',');
-  if (values.length < 3) {
-    const nums = values.map(getNum);
-    return nums.reduce((ac, cv) => ac + cv);
-  };
+  const nums = values.map(getNum);
+  return nums.reduce((ac, cv) => ac + cv);
 };
 
 export default calculate;
